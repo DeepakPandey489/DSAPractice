312c5adacac328fd7becfc6ac5883219f7755838
Deepak <deepakwebearn2528@gmail.com>
2026-02-14 12:00:54 +0530

    Added TwoPointer pair Sum Location

---
+++ practice.twoPointer/src/practice/TwoPointer/SumOfArrayByBruteForce.java
@@ -0,0 +1,18 @@
+package practice.TwoPointer;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SumOfArrayByBruteForce {
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 3 };
+
+		int[] arr = { 1, 2, 3, 4, 5 };
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+		}
+		System.out.println(sum);
+	}
+}
